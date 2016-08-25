d6ddb85823d0a504f644c9259bad449ff3784f34
Saumitra Bose <imtoobose@gmail.com>
2016-08-25 18:26:39 +0530

    Better animation for link icon

--- dev/js/worksscroll.js
+++ dev/js/worksscroll.js
@@ -9,19 +9,36 @@ module.exports= () => {
 
 var animateUp = ($out, $in, callback) => {
   var t= new TimelineLite();
+
+  var
+  $indesc  = ($in.getElementsByClassName('workdescription'))[0],
+  $outdesc = ($out.getElementsByClassName('workdescription'))[0];
+
   t
   .set($in, {y: '-100%', autoAlpha:1})
+  .set($indesc, {autoAlpha: 0})
   .to($out, 0.5, {y: '100%'}, 0)
-  .to($in, 0.5, {y: '+=100%'}, 0);
+  .to($in, 0.5, {y: '+=100%'}, 0)
+  .to($outdesc, 0.3, {autoAlpha: 0}, 0.2)
+  .to($indesc, 0.3, {autoAlpha: 1}, 0.2);
+  
   callback();
 }
 
 var animateDown = ($out, $in, callback) => {
   var t= new TimelineLite();
+  var
+  $indesc  = ($in.getElementsByClassName('workdescription'))[0],
+  $outdesc = ($out.getElementsByClassName('workdescription'))[0];
+
   t
   .set($in, {y: '100%', autoAlpha: 1})
+  .set($indesc, {autoAlpha: 0})
   .to($out, 0.5, {y: '-100%'}, 0)
-  .to($in, 0.5, {y: '-=100%'}, 0);
+  .to($in, 0.5, {y: '-=100%'}, 0)
+  .to($outdesc, 0.3, {autoAlpha: 0}, 0)
+  .to($indesc, 0.3, {autoAlpha: 1}, 0.2);
+  
   callback();
 }
 
